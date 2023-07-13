26d3c93d895ac45d6c6d68ef9e6ef7ac94315255
Ahmad Alsaleh <ahmed.asaleh2@gmail.com>
2023-07-13 08:09:07 +0400

    Fixing WAM & BAM implementation. The function is now called aggregate.scores()

--- Codes/Part 2 - Feature Selection/Part 2 - Feature Selection.R
+++ Codes/Part 2 - Feature Selection/Part 2 - Feature Selection.R
@@ -43,84 +43,15 @@ data$Diagnosis = as.factor(data$Diagnosis)
 colnames(data)[ncol(data)] = "y"
 str(data[, (ncol(data) - 2):ncol(data)])
 
-# requirements for the wam() function ---
+# feature selection ----
+aggregated.scores = aggregate.scores(data)
 
-# Feature Selection Part 1: Filter methods ----
-aggregated.scores = wam(data)
+# sorting OTUs by importance scores of each method ----
+top.features = matrix(nrow = nrow(aggregated.scores), ncol = ncol(aggregated.scores))
+colnames(top.features) = colnames(aggregated.scores)
 
-
-# TODO (Dr. Ayman) check if this is correct:
-"
-In WAM, scores of Random Forest is not included in WAB since it is not a filter
-method but it is included in the BAM. (I have a feeling this is wrong).
-
-"
-
-# Feature Selection Part 2: Embedded and wrapper methods (machine learning) ----
-
-# Random Forest ---
-cv.folds = createFolds(data$y, k = 5, returnTrain = T)
-training.control = trainControl(
-	method = "cv",
-	number = 5,
-	search = "grid",
-	classProbs = T,
-	savePredictions = "final",
-	index = cv.folds,
-	summaryFunction = twoClassSummary
-)
-
-# fitting a random forest model with custom tuning parameters
-rf.model = train(
-	y ~ .,
-	data = data,
-	method = "rf",
-	metric = "ROC",
-	tuneGrid = expand.grid(.mtry = c(5, 25, 50, 100, 200, 300)),
-	trControl = training.control,
-	importance = T,
-	nodesize = 1,
-	ntree = 250,
-	allowParallel = T
-)
-rf.model
-
-# plotting ROC curve
-roc.plot = ggplot(rf.model$pred[rf.model$pred$mtry == rf.model$finalModel$mtry,],
-									aes(m = Normal, d = factor(obs, levels = c("Normal", "CRC")))) +
-	geom_roc(n.cuts = 0) + coord_equal() + style_roc()
-roc.plot = roc.plot + annotate("text", x = 0.75, y = 0.25, label =
-															 	paste("AUC =", round((calc_auc(roc.plot))$AUC, 4)))
-roc.plot
-
-# plotting variables importance
-var.imp.rf = varImp(rf.model)
-plot(var.imp.rf, top = 20)
-
-# extracting top variables from random forest model
-var.imp.rf = var.imp.rf$importance[rownames(aggregated.scores),][, 1]
-aggregated.scores = data.frame(aggregated.scores, `Random Forest` = var.imp.rf)
-
-# BAM (aggregating using arithmetic mean) ----
-
-# normalizing scores
-aggregated.scores = as.data.frame(apply(aggregated.scores, 2, normalize))
-
-# TODO (Dr. Ayman): is this how BAM should be computed? I am basically computing the 
-# mean of all WAM scores. But note that I normalized before computing the mean.
-
-# computing BAM
-aggregated.scores$BAM = apply(aggregated.scores, 1, mean)
-
-# sorting by score for each method ----
-top.features = matrix(nrow = nrow(aggregated.scores),
-											ncol = ncol(aggregated.scores))
 for (col_i in 1:ncol(aggregated.scores))
-	top.features[, col_i] = rownames(aggregated.scores[order(aggregated.scores[, col_i],
-																													 decreasing = T),])
-
-top.features = as.data.frame(top.features)
-colnames(top.features) = c("IG", "SU", "MR", "CS", "RF", "BAM")
+	top.features[, col_i] = rownames(aggregated.scores)[order(-aggregated.scores[, col_i])]
 
 # exporting results ----
 write.xlsx(aggregated.scores, "Output Datasets/Bootstrapped Importance Scores.xlsx")
--- Codes/Part 2 - Feature Selection/utility.R
+++ Codes/Part 2 - Feature Selection/utility.R
@@ -6,41 +6,39 @@
 # Preconditions:
 # - The target variable is named "y"
 # - `iteration_i` must increase in each iteration as it is used for setting the seed
-single.wam_ = function(data, iteration_i, num_bootstraps) {
+single.bootstrap_ = function(data, iteration_i, num_bootstraps) {
 	# using different seed for each bootstrap
 	set.seed(iteration_i)
 	
 	# getting stratified sample
 	d = as.data.frame(stratified(data, "y", size = 0.999, replace = T))
 	
+	# importance scores of the features
+	scores = matrix(nrow = ncol(d) - 1, ncol = 5, dimnames =
+										list(colnames(d)[1:(ncol(d)-1)], c("IG", "SU", "MR", "CS", "BAM")))
+	
 	# Information Gain (IG)
-	scores = information.gain(y ~ ., d)
-	colnames(scores)[ncol(scores)] = "Information.Gain"
+	scores[, "IG"] = information.gain(y ~ ., d)[, 1]
 	
 	# Symmetric Uncertainty (SU)
-	scores = cbind(scores, symmetrical.uncertainty(y ~ ., d))
-	colnames(scores)[ncol(scores)] = "Symmetric.Uncertainty"
+	scores[, "SU"] = symmetrical.uncertainty(y ~ ., d)[, 1]
 	
-	# Minimum Redundancy Maximum Relevance (MRMR)
-	d.mrmr = MRMR(d[,!(colnames(d) %in% c("y"))], d$y, ncol(d) - 1)$score
-	scores = merge(scores, d.mrmr, by = "row.names", all = T)
-	scores = scores %>%
-		remove_rownames() %>%
-		column_to_rownames(var = "Row.names")
-	colnames(scores)[ncol(scores)] = "MRMR"
+	# Minimum Redundancy Maximum Relevance (MR)
+	scores[, "MR"] = MRMR(d[, -ncol(d)], d$y, ncol(d) - 1)$score
 	
 	# Chi-Squared (CS)
-	d.chi = chi.squared(y ~ ., d)
-	scores = merge(scores, d.chi, by = "row.names", all = T)
-	scores = scores %>%
-		remove_rownames() %>%
-		column_to_rownames(var = "Row.names")
-	colnames(scores)[ncol(scores)] = "Chi.Squared"
+	scores[, "CS"] = chi.squared(y ~ ., d)[, 1]
+	
+	# normalizing
+	scores[, 1:4] = apply(scores[, 1:4], 2, normalize)
+	
+	# BAM
+	scores[, "BAM"] = apply(scores[, 1:4], 1, mean)
 	
 	# printing updated progress
 	done_percent = round(iteration_i / num_bootstraps * 100, 1)
-	text = paste("Iteration: ", iteration_i, "/", num_bootstraps, " (",
-							 done_percent, "%)", sep = "")
+	text = paste0("Iteration: ", iteration_i, "/", num_bootstraps, " (",
+							 done_percent, "%)")
 	print(text)
 	
 	# return importance scores of a single bootstrap
@@ -54,19 +52,17 @@ normalize = function(x) {
 
 # performs WAM on `data` ----
 # Preconditions:
-# - The target variable is named "y"
-# TODO (Dr. Ayman):
-# Please check if WAM is implemented correctly, especially if
-# the aggreagation and normalization are done in the right place.
-# I am using `apply(bootstraps, 1:2, mean)` to aggreagate the scores of each bootstrap. 
+# - The target variable should be the last column in `data` and should be named "y"
 
-wam = function(data, num_bootstraps = 500) {
+# TODO: rename the functions (wam and single.wam)
+
+aggregate.scores = function(data, num_bootstraps = 500) {
 	iteration_i = 0
 	
 	# bootstrapping `num_bootstraps` times
 	bootstraps = replicate(num_bootstraps, simplify = F, expr = { # TODO (Ahmad Alsaleh): try to use multi-core processing for replicate()
 		iteration_i <<- iteration_i + 1
-		single.wam_(data, iteration_i, num_bootstraps)
+		single.bootstrap_(data, iteration_i, num_bootstraps)
 	}) %>% abind::abind(along = 3)
 	
 	# aggreagating scores of all bootstraps using the arithmatice mean
